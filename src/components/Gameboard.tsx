70912a9e73958579ea11e73048a70a5d2d7df4d0
NaN <ccmolinac@gmail.com>
2022-02-27 03:19:05 -0500

    GAMEBOARD COMPONENT Updated

--- src/components/Gameboard.tsx
+++ src/components/Gameboard.tsx
@@ -10,8 +10,26 @@ class Gameboard extends React.Component<any, any> {
 
   static getDerivedStateFromProps(props: any) {
     return {
-      word: props.word.split('').map( (char: String)=> {
-        return <div className="w-[75px] h-[75px] bg-gray-darker flex justify-center items-center rounded uppercase">{char}</div>
+      word: [...props.word,...Array(25 - props.word.length)].map( (char, index) => {
+        let state = 'bg-gray'; //char ? `bg-state-${char.state}` : `bg-gray`;
+        if(char) {
+          switch (char.state) {
+            case 0:
+              state = "!bg-state-0"
+              break;
+            case 1:
+              state = "!bg-state-1"
+              break;
+            case 2:
+              state = "!bg-state-2"
+              break;
+            default:
+              break;
+          }
+        }
+        return <div key={index} className={`w-[75px] h-[75px] flex justify-center items-center rounded dark:bg-d-gray transition duration-500 ${state}`}>
+          <span className="text-4xl text-white font-extrabold">{char ? char.value: ''}</span>
+        </div>
       })
     }
   }
@@ -22,8 +40,10 @@ class Gameboard extends React.Component<any, any> {
 
   render() {
     return (
-      <div className="h-[562px] w-full flex justify-center items-center space-x-3">
-        {this.state.word}
+      <div className="Gameboard h-[562px] w-full flex justify-center items-center">
+        <div className="h-fit w-fit grid grid-cols-5 grid-flow-row gap-3">
+          {this.state.word}
+        </div>
       </div>
     )
   }
