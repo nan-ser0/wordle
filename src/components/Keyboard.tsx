788461e718dd7ec23f0353c5e3fb5aac2ffaeeba
NaN <ccmolinac@gmail.com>
2022-02-27 17:59:35 -0500

    KEYBOARD COMPONENT Updated

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 class Keyboard extends React.Component {
   render() {
     return (
-      <div className='Keyboard flex flex-col items-center bg-gray w-full h-fit rounded-2xl px-14 py-8 dark:bg-d-gray transition duration-500'>
+      <div className='Keyboard flex flex-col items-center bg-gray w-full-custom h-fit rounded-2xl px-14 py-8 dark:bg-d-gray transition duration-500'>
         {this.props.children}
       </div>
     )
