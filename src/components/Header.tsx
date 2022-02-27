7e31c854e14949a11f58c2916436d851e43d75b5
NaN <ccmolinac@gmail.com>
2022-02-27 17:59:45 -0500

    HEADER COMPONENT Updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,7 @@ class Header extends React.Component<any, any> {
   };
   render() {
     return (
-      <div className="Header flex items-center justify-between bg-gray w-full h-[84px] rounded-2xl px-12 py-4 dark:bg-d-gray transition duration-500">
+      <div className="Header flex items-center justify-between bg-gray w-full-custom h-[84px] rounded-2xl px-12 py-4 dark:bg-d-gray transition duration-500">
         <div className="Header__instructions">
           {/* <img className="Header__instructions-icon" src="assets/icons/instructions.svg" alt="" /> */}
           {this.props.children}
