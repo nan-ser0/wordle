27f9b8302c849e0a7173f0d6049bd22461322671
NaN <ccmolinac@gmail.com>
2022-02-27 20:06:47 -0500

    HEADER COMPONENT Updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,36 @@
 import React from "react";
 
 class Header extends React.Component<any, any> {
-  // constructor(props: any) {
-  //   super(props);
-  //   this.toggleMode = this.toggleMode.bind(this);
-  // }
+  constructor(props:any) {
+    super(props)
+    this.state = {
+      posX: 0,
+      classToggle: `w-[27px] h-[27px] bg-gray-lighter dark:bg-d-gray-lighter border border-solid border-dark dark:border-gray-lighter rounded-full absolute inset-0 transition duration-500`
+    }
+
+    this.toggleMode = this.toggleMode.bind(this)
+  }
   toggleMode() {
     const root = document.getElementsByTagName( 'html' )[0];
     root.classList.toggle('dark');
+    this.setState({
+      posX: this.state.posX === 0 ? 100 : 0,
+      classToggle: this.state.posX === 0 ? `w-[27px] h-[27px] bg-gray-lighter dark:bg-d-gray-lighter border border-solid border-dark dark:border-gray-lighter rounded-full absolute inset-0 transition duration-500 translate-x-[100%]` : `w-[27px] h-[27px] bg-gray-lighter dark:bg-d-gray-lighter border border-solid border-dark dark:border-d-gray-lighter rounded-full absolute inset-0 transition duration-500 translate-x-[0]`
+    })
   };
   render() {
     return (
       <div className="Header flex items-center justify-between bg-gray w-full-custom h-[84px] rounded-2xl px-12 py-4 dark:bg-d-gray transition duration-500">
         <div className="Header__instructions">
-          {/* <img className="Header__instructions-icon" src="assets/icons/instructions.svg" alt="" /> */}
           {this.props.children}
         </div>
         <h1 className="Header__title text-5xl text-dark font-semibold dark:text-white transition duration-500">
           WORDLE
         </h1>
         <div className="Header__theme-mode">
-          <button onClick={this.toggleMode}>mode</button>
+          <button onClick={this.toggleMode} className="bg-dark dark:bg-gray-lighter w-[54px] h-[27px] rounded-[27px] relative transition duration-500">
+            <div className={this.state.classToggle}></div>
+          </button>
         </div>
       </div>
     )
