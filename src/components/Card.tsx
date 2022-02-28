0bd68c94e95bba22ed885f2fdcfb258a4aa16c37
NaN <ccmolinac@gmail.com>
2022-02-27 22:16:48 -0500

    CARD COMPONENT Updated

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,7 +4,7 @@ class Card extends React.Component<any, any> {
   render() {
     return (
       <div
-        className={`w-[75px] h-[75px] flex justify-center items-center rounded dark:bg-d-gray transition duration-500 ${this.props.classType}`}
+        className={`w-[50px] h-[50px] 2xl:w-[75px] 2xl:h-[75px] flex justify-center items-center rounded dark:bg-d-gray transition duration-500 ${this.props.classType}`}
       >
         <span className="text-4xl text-white font-extrabold">
           {this.props.char}
