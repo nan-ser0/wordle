04b40ff5f0260b26d08f78763814fedc0f1d04c2
NaN <ccmolinac@gmail.com>
2022-02-27 22:16:16 -0500

    KEY COMPONENT Updated

--- src/components/Key.tsx
+++ src/components/Key.tsx
@@ -12,7 +12,7 @@ class Key extends React.Component<any, any> {
 
   render() {
     return (
-      <button onClick={this.getChar} className='Key group flex justify-center items-center bg-gray-darker w-[45px] h-[50px] rounded m-1.5 cursor-pointer dark:bg-d-gray-lighter hover:bg-dark dark:hover:bg-dark transition duration-500'>
+      <button onClick={this.getChar} className='Key group flex justify-center items-center bg-gray-darker w-[25px] h-[30px] lg:w-[45px] lg:h-[50px] rounded m-0.5 lg:m-1.5 cursor-pointer dark:bg-d-gray-lighter hover:bg-dark dark:hover:bg-dark transition duration-500'>
         <span className='Key__label text-dark font-semibold dark:text-white group-hover:text-white transition duration-500'>{this.props.label}</span>
       </button>
     )
